b2594d051969a58203664be020e7b867cbb60ab3
simsungbo <94431627+Sim-918@users.noreply.github.com>
2022-05-30 13:34:35 +0900

    Create ARC4.py

---
+++ secure/ARC4.py
@@ -0,0 +1,30 @@
+from Crypto.Cipher import ARC4
+from Crypto.Hash import SHA256 as SHA
+
+class myARC4():
+    def __init__(self,keytext):
+        self.key=keytext.encode()
+
+    def enc(self,plaintext):
+        arc4=ARC4.new(self.key)
+        encmsg=arc4.encrypt(plaintext.encode())
+        return encmsg
+    
+    def dec(self,ciphertext):
+        arc4=ARC4.new(self.key)
+        decmsg=arc4.decrypt(ciphertext)
+        return decmsg
+    
+def main():
+    keytext='samsjang'
+    msg='ilovepythov'
+
+    myCipher=myARC4(keytext)
+    ciphered=myCipher.enc(msg)
+    deciphered=myCipher.dec(ciphered)
+    
+    print('Original:\t%s'%msg)
+    print('Ciphered:\t%s'%ciphered)
+    print('Decripher:\t%s'%deciphered)
+
+main()
