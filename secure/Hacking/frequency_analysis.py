02da54a627171d1877dcf816d6037228f0828324
simsungbo <94431627+Sim-918@users.noreply.github.com>
2022-06-05 14:00:11 +0900

    Create frequency_analysis.py

---
+++ secure/Hacking/frequency_analysis.py
@@ -0,0 +1,13 @@
+############frequency_analysis############
+# def frequency_analysis(msg):
+#     fa={}
+#     for c in msg:
+#         if c in fa:
+#             fa[c]+=1
+#         else:
+#             fa[c]=1
+#     print(sorted(fa.items(),key=lambda x:x[1],reverse=True))
+
+# if __name__=='__main__':
+#     msg='asdashglcxmbnvbgn;ghdsa;fj#!@%&%@#%&^%*^@$qweqsadasfjklczxxcb%#^@#$@#$'
+#     frequency_analysis(msg)
