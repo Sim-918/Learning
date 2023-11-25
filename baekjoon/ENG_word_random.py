06a0c2ad9f60cbe03194a0b48182393fdcae3a71
simsungbo <94431627+Sim-918@users.noreply.github.com>
2023-11-25 14:34:58 +0900

    Update ENG_word_random.py

--- baekjoon/ENG_word_random.py
+++ baekjoon/ENG_word_random.py
@@ -1,64 +1,24 @@
 import random
 
-word=[['a','에이'],['b','비'],['c','씨'],['d','디'],['e','이'],['f','에프'],]
+word=[['a','에이'],['b','비'],['c','씨'],['d','디'],['e','이'],['f','에프']]
+sunji_list=word*2
+print(sunji_list)
 
-sunji=random.sample(range(len(word)),4)
-sunji_arr=[]
+cnt=0
 
-# 선지를 word전체에서 가져옴
-for i in sunji:
-    sunji_arr.append(word[i])
-
-
-# 다음 루프 때 만약 ans_arr가 not in하면 루프반복
-# 있으면 다음 랜덤 찾기
-# len(word)와 len(ans)의 길이가 같아지면 반복문 종료
-ans=random.sample(sunji_arr,1)
-# ans_arr=[]
-# if ans in ans_arr:
-#     print("겹침")
-
-print(f"문제-->",ans[0][0])
-
-# ans_arr.append(ans[0][0])
-# print(ans_arr)
-
-
-for i in range(len(sunji_arr)):
-    print(f"{i+1}",sunji_arr[i][1])
-
-user_input=int(input("정답: "))-1    # 4
-
-if sunji_arr[user_input][1]==ans[0][1]:
-    print("correct")
-else:
-    print("you dead")
-
-
-
-
-
-
-
-
-
-# while True:
-#     test=random.sample(word,4)
-#     sunji=random.randint(0,3)
-#     tmp=test[sunji][0]
-
-#     print(tmp,"의 정답은?")
-#     for i in range(4):
-#         print(f"{i+1}",test[i][1])
-
-#     user_input=int(input("답: "))-1
-
-#     if user_input==-1:
-#         break
-#     if sunji==user_input:
-#         print("정답")
-#     else:
-#         print("오답")
-
-    
-
+for i in range(len(word)):
+    test=[word[i][1]]
+    print(f"문제: ",word[i][0])
+    tmp=sunji_list[i+1:i+4]
+    for j in tmp:
+        test.append(j[1])
+    random.shuffle(test)
+    print(test)
+    user_input=int(input())
+    if word[i][1]==test[user_input-1]:
+        cnt+=1
+        print("correct")
+    else:
+        print("you dead")
+print(f"문항수 : ",len(word))
+print(f"맞춘 수: ",cnt)
