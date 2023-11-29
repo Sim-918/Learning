9bc2228cfa9896a83b2693c47d207989a29cf595
simsungbo <94431627+Sim-918@users.noreply.github.com>
2023-11-29 18:34:48 +0900

    Update ENG_word_random.py

--- baekjoon/ENG_word_random.py
+++ baekjoon/ENG_word_random.py
@@ -1,70 +1,43 @@
 import random
 
-word=[
-    ['resume', '이력서'],
-    ['opening', '개장, 공석'], 
-    ['applicant', '지원자'], 
-    ['requirement', '필요조건'],
-    ['meet', '만족시키다'],
-    ['qualified', '적격의,자격있는'],
-    ['candidate', '후보자'],
-    ['confidence', '확신'],
-    ['highly', '매우'],
-    ['professional', '전문적인'],
-    ['interview', '면접'],
-    ['hire', '고용'],
-    ['training', '교육'],
-    ['reference', '참조'],
-    ['position', '일자리'],
-    ['achievement', '성취'],
-    ['impressed', '감명을 받은'],
-    ['excellent', '훌룡한'],
-    ['eligible', '자격이있는'],
-    ['identify', '찾아내다, 확인하다'],
-    ['associate', '관련시키다'],
-    ['condition', '조건'],
-    ['employment', '고용'],
-    ['lack', '~이 없다'],
-    ['managerial', '관리의'],
-    ['diligent', '성실한'],
-    ['familiar', '익숙한'],
-    ['proficiency', '능숙함'],
-    ['prospective', '장래의'],
-    ['appeal', '매력, 주목을 끌다'],
-    ['specialize', '전문화하다'],
-    ['apprehensive', '걱정하는'],
-    ['consultant', '상담자,조언자'],
-    ['entitle', '자격 부여하다'],
-    ['degree', '학위'],
-    ['payroll', '급료, 임금대장'],
-    ['recruit', '모집하다'],
-    ['certification', '증명성'],
-    ['occupation', '직업'],
-    ['wage', '임금, 급료']
-]
+word = []
+# 문자열 정리 코드
+while True:
+    input_1 = input("done 입력 시 종료: ")
+    if input_1.lower() == 'done':
+        break
+    
+    word_translation = [x.strip() for x in input_1.split('\t', 1)]
+    
+    word.append(word_translation)
+
 sunji_list=word*2
 
-cnt=0
+correct_cnt=0
+now_cnt=len(word)
 
 # 틀린문제
 X=[]
 
 for i in range(len(word)):
+    print('-'*10)
+    print("남은 문항 수:",now_cnt)
+    now_cnt-=1
     test=[word[i][1]]
     print(f"문제: ",word[i][0])
     tmp=sunji_list[i+1:i+4]
     for j in tmp:
         test.append(j[1])
     random.shuffle(test)
-    print(test)
-    user_input=int(input())
+    for p in range(len(test)):
+        print(p+1,test[p])
+    user_input=int(input("정답: "))
     if word[i][1]==test[user_input-1]:
-        cnt+=1
-        print("correct")
+        correct_cnt+=1
     else:
         X.append(word[i])
-        print("worn")
 
-print(f"문항수 : ",len(word))
-print(f"맞춘 수: ",cnt)
+
+print(f"총 문항수 : ",len(word),end=' ')
+print(f"맞춘 수: ",correct_cnt)
 print("틀린문제",X)
