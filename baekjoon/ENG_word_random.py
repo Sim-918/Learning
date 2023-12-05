1de86526bdd4f687850851251187c28114ee43e4
simsungbo <94431627+Sim-918@users.noreply.github.com>
2023-12-05 19:26:26 +0900

    Update ENG_word_random.py

--- baekjoon/ENG_word_random.py
+++ baekjoon/ENG_word_random.py
@@ -11,27 +11,35 @@ while True:
     
     word.append(word_translation)
 
-sunji_list=word*2
+sunji_list=word+word[:3]
 
 correct_cnt=0
 now_cnt=len(word)
+
 
 # 틀린문제
 X=[]
 
 for i in range(len(word)):
-    print('-'*10)
+    print('-'*20)
     print("남은 문항 수:",now_cnt)
     now_cnt-=1
+
+    # 문제 생성
     test=[word[i][1]]
     print(f"문제: ",word[i][0])
+
+    # 선지 담기
     tmp=sunji_list[i+1:i+4]
     for j in tmp:
         test.append(j[1])
     random.shuffle(test)
+    # 문제 출력
     for p in range(len(test)):
         print(p+1,test[p])
+    # 정답 입력 받기
     user_input=int(input("정답: "))
+    
     if word[i][1]==test[user_input-1]:
         correct_cnt+=1
     else:
